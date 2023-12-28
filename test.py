d530240fd760759021e84df5290edede48d793c7
sulincix <parduscix@yandex.ru>
2023-12-28 00:08:54 +0000

    update

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
-import os
-os.environ["GI_TYPELIB_PATH"] = os.path.dirname(__file__)+"/build/"
-os.environ["LD_LIBRARY_PATH"] = os.path.dirname(__file__)+"/build/"
-
+#!/usr/bin/env python3
 import gi
 gi.require_version("Example", "1.0")
 from gi.repository import Example
